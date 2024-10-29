fe3735b0fa50bdef7021bbed6454d9d53602a3df
Sam Bailey <samcb@google.com>
2024-10-29 09:23:11 -0700

    Add data utils for exploding and collapsing headline and descriptions.
    PiperOrigin-RevId: 691047340
    Change-Id: Ib1e60dcf232ed9acac326ab99618ee5a07ec37f4

---
+++ py/copycat/data/utils.py
@@ -0,0 +1,158 @@
+# Copyright 2024 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utility functions for working with data."""
+
+from typing import Any, Callable
+
+import pandas as pd
+
+
+def collapse_headlines_and_descriptions(
+    data: pd.DataFrame,
+) -> pd.DataFrame:
+  """Collapses headline and description columns into two new columns.
+
+  Assumes that the headline and description column names have the format
+  "Headline {i}" and "Description {i}", where {i} is the headline number, with
+  a single headline or description per column.
+
+  Then collapses them into two new columns, "headlines" and "descriptions",
+  containing lists of headlines and descriptions respectively.
+
+  Args:
+    data: The input DataFrame.
+
+  Returns:
+    A new DataFrame with the headline and description columns collapsed into two
+    new columns, "headlines" and "descriptions".
+  """
+  output_data = data.copy()
+
+  headline_cols = [
+      c
+      for c in output_data.columns
+      if c.startswith("Headline ")
+      and c.split(" ")[1].isdigit()
+      and len(c.split(" ")) == 2
+  ]
+  description_cols = [
+      c
+      for c in output_data.columns
+      if c.startswith("Description ")
+      and c.split(" ")[1].isdigit()
+      and len(c.split(" ")) == 2
+  ]
+  output_data["headlines"] = pd.Series(
+      {
+          k: list(
+              filter(lambda x: x != "--" and x, v),
+          )
+          for k, v in output_data[headline_cols].T.to_dict("list").items()
+      },
+      index=output_data.index,
+  )
+  output_data["descriptions"] = pd.Series(
+      {
+          k: list(filter(lambda x: x != "--" and x, v))
+          for k, v in output_data[description_cols].T.to_dict("list").items()
+      },
+      index=output_data.index,
+  )
+
+  output_data = output_data.drop(columns=headline_cols + description_cols)
+
+  return output_data
+
+
+def _explode_to_columns(output_name: str) -> Callable[[list[Any]], pd.Series]:
+  """Returns a function that explodes a list into a Series of columns.
+
+  The returned function takes a list as input and returns a Series where each
+  element of the list is assigned to a separate column, with the column names
+  having the format f"{output_name} {i}", where i is the index of the element
+  in the list and starts at 1.
+
+  Args:
+    output_name: The name of the output columns.
+
+  Returns:
+    A function that explodes a list into a Series of columns.
+  """
+
+  def apply_explode_to_columns(list_col: list[Any]) -> pd.Series:
+    if not isinstance(list_col, list):
+      raise ValueError(
+          "The input to the explode_to_columns function must be a list, got"
+          f" {type(list_col)} instead."
+      )
+    return pd.Series(
+        list_col,
+        index=[f"{output_name} {i+1}" for i in range(len(list_col))],
+    )
+
+  return apply_explode_to_columns
+
+
+def explode_headlines_and_descriptions(data: pd.DataFrame) -> pd.DataFrame:
+  """Explodes headline and description columns into separate columns.
+
+  Assumes that the headline and description columns have been collapsed into two
+  new columns, "headlines" and "descriptions", containing lists of headlines and
+  descriptions respectively.
+
+  Then explodes them into separate columns, with the column names having the
+  format "Headline {i}" and "Description {i}", where {i} is the index of the
+  headline or description and starts at 1.
+
+  Args:
+    data: The input DataFrame.
+
+  Returns:
+    A new DataFrame with the headline and description columns exploded into
+    separate columns.
+
+  Raises:
+    ValueError: If the index of the input data is not unique.
+  """
+  if not data.index.is_unique:
+    raise ValueError(
+        "The index of the input data is not unique, cannot explode headlines"
+        " and descriptions."
+    )
+
+  if "headlines" in data:
+    headlines = (
+        data["headlines"].apply(_explode_to_columns("Headline")).fillna("--")
+    )
+  else:
+    headlines = pd.DataFrame()
+
+  if "descriptions" in data:
+    descriptions = (
+        data["descriptions"]
+        .apply(_explode_to_columns("Description"))
+        .fillna("--")
+    )
+  else:
+    descriptions = pd.DataFrame()
+
+  output_data = (
+      data.copy()
+      .drop(columns=["headlines", "descriptions"])
+      .merge(headlines, left_index=True, right_index=True)
+      .merge(descriptions, left_index=True, right_index=True)
+  )
+
+  return output_data
---
+++ py/copycat/data/utils_test.py
@@ -0,0 +1,169 @@
+# Copyright 2024 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import pandas as pd
+
+from copycat.data import utils
+
+
+class ExplodeAndCollapseHeadlinesAndDescriptionsTest(parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="with_headlines_and_descriptions",
+          data=pd.DataFrame({
+              "Headline 1": ["a", "b"],
+              "Headline 2": ["c", "--"],
+              "Headline 3": ["d", ""],
+              "Description 1": ["e", "f"],
+              "Description 2": ["g", "--"],
+              "Other column": [1, 2],
+          }),
+          expected=pd.DataFrame({
+              "headlines": [["a", "c", "d"], ["b"]],
+              "descriptions": [["e", "g"], ["f"]],
+              "Other column": [1, 2],
+          }),
+      ),
+      dict(
+          testcase_name="with_no_headlines",
+          data=pd.DataFrame({
+              "Description 1": ["e", "f"],
+              "Description 2": ["g", "--"],
+              "Other column": [1, 2],
+          }),
+          expected=pd.DataFrame({
+              "headlines": [[], []],
+              "descriptions": [["e", "g"], ["f"]],
+              "Other column": [1, 2],
+          }),
+      ),
+      dict(
+          testcase_name="with_no_descriptions",
+          data=pd.DataFrame({
+              "Headline 1": ["a", "b"],
+              "Headline 2": ["c", "--"],
+              "Headline 3": ["d", ""],
+              "Other column": [1, 2],
+          }),
+          expected=pd.DataFrame({
+              "headlines": [["a", "c", "d"], ["b"]],
+              "descriptions": [[], []],
+              "Other column": [1, 2],
+          }),
+      ),
+      dict(
+          testcase_name="with_no_headlines_or_descriptions",
+          data=pd.DataFrame({"Other column": [1, 2]}),
+          expected=pd.DataFrame({
+              "headlines": [[], []],
+              "descriptions": [[], []],
+              "Other column": [1, 2],
+          }),
+      ),
+  )
+  def test_collapse_headlines_and_descriptions(self, data, expected):
+    actual = utils.collapse_headlines_and_descriptions(data)
+    pd.testing.assert_frame_equal(actual, expected, check_like=True)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="with_headlines_and_descriptions",
+          data=pd.DataFrame({
+              "headlines": [["a", "c", "d"], ["b"]],
+              "descriptions": [["e", "g"], ["f"]],
+              "Other column": [1, 2],
+          }),
+          expected=pd.DataFrame({
+              "Headline 1": ["a", "b"],
+              "Headline 2": ["c", "--"],
+              "Headline 3": ["d", "--"],
+              "Description 1": ["e", "f"],
+              "Description 2": ["g", "--"],
+              "Other column": [1, 2],
+          }),
+      ),
+      dict(
+          testcase_name="with_no_headlines",
+          data=pd.DataFrame({
+              "headlines": [[], []],
+              "descriptions": [["e", "g"], ["f"]],
+              "Other column": [1, 2],
+          }),
+          expected=pd.DataFrame({
+              "Description 1": ["e", "f"],
+              "Description 2": ["g", "--"],
+              "Other column": [1, 2],
+          }),
+      ),
+      dict(
+          testcase_name="with_no_descriptions",
+          data=pd.DataFrame({
+              "headlines": [["a", "c", "d"], ["b"]],
+              "descriptions": [[], []],
+              "Other column": [1, 2],
+          }),
+          expected=pd.DataFrame({
+              "Headline 1": ["a", "b"],
+              "Headline 2": ["c", "--"],
+              "Headline 3": ["d", "--"],
+              "Other column": [1, 2],
+          }),
+      ),
+      dict(
+          testcase_name="with_no_headlines_or_descriptions",
+          data=pd.DataFrame({
+              "headlines": [[], []],
+              "descriptions": [[], []],
+              "Other column": [1, 2],
+          }),
+          expected=pd.DataFrame({"Other column": [1, 2]}),
+      ),
+  )
+  def test_explode_headlines_and_descriptions(self, data, expected):
+    actual = utils.explode_headlines_and_descriptions(data)
+    pd.testing.assert_frame_equal(actual, expected, check_like=True)
+
+  def test_explode_headlines_and_descriptions_raises_value_error_if_index_not_unique(
+      self,
+  ):
+    data = pd.DataFrame({
+        "headlines": [["a", "c", "d"], ["b"]],
+        "descriptions": [["e", "g"], ["f"]],
+        "Other column": [1, 1],
+    }).set_index("Other column")
+    with self.assertRaises(ValueError):
+      utils.explode_headlines_and_descriptions(data)
+
+  @parameterized.parameters(
+      "headlines",
+      "descriptions",
+  )
+  def test_explode_headlines_and_descriptions_raises_value_error_if_headlines_or_descriptions_are_not_lists(
+      self, column_name
+  ):
+    data = pd.DataFrame({
+        "headlines": [["a", "c", "d"], ["b"]],
+        "descriptions": [["e", "g"], ["f"]],
+        "Other column": [1, 1],
+    })
+    data[column_name] = ["a", "b"]  # Column does not contain lists.
+    with self.assertRaises(ValueError):
+      utils.explode_headlines_and_descriptions(data)
+
+
+if __name__ == "__main__":
+  absltest.main()
